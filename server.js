4cc46062575dee1198547f6fe2d76221e9fd6a2c
Anthony Ng <newyork.anthonyng@gmail.com>
2020-09-17 19:12:43 -0400

    set up Heroku deployment

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ io.on('connection', socket => {
   });
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 http.listen(PORT, () => {
   console.log(`Server listening on port: ${PORT}`);
 });
